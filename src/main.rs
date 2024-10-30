8f5bc21d41ae9d310a43ae46ac47d2e0813a7e68
pavel <pavel_vilkov17@mail.ru>
2024-10-30 16:43:22 +0700

    добавил лазер

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use std::time::Duration;
 
 const GPIO_BUTTON: u8 = 17;
 
+const GPIO_PIN_LAZER: u8 = 18;
+
+fn blink(pin: &mut rppal::gpio::OutputPin, count: u32, duration: Duration) {
+    for _ in 0..count {
+        pin.set_high();
+        thread::sleep(duration);
+        pin.set_low();
+        thread::sleep(duration);
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Флаг для корректного завершения программы
     let running = Arc::new(AtomicBool::new(true));
@@ -20,6 +31,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let gpio = Gpio::new()?;
     let mut button = gpio.get(GPIO_BUTTON)?.into_input_pullup();
 
+    let mut pin = gpio.get(GPIO_PIN_LAZER)?.into_output();
+
     // Настраиваем прерывание на falling edge (нажатие кнопки)
     button.set_interrupt(Trigger::FallingEdge)?;
 
@@ -30,6 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         match button.poll_interrupt(true, Some(Duration::from_millis(100)))? {
             Some(Level::Low) => {
                 println!("Кнопка нажата!");
+                blink(&mut pin, 5, Duration::from_millis(100));
                 // Добавляем задержку для устранения дребезга контактов
                 thread::sleep(Duration::from_millis(300));
             },
